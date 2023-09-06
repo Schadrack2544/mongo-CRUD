9b55276649a29d1cdc442d8703a7c84057de09cc
Schadrack2544 <nzayinaschad250@gmail.com>
2023-09-06 12:29:22 +0200

    resolving issues by using promises

--- index.js
+++ index.js
@@ -21,89 +21,107 @@ connection
 
 const student = connection.db("school").collection("students");
 //CRUD Operations
-connection.close();
 
 const app = express();
 const port = 4000;
 
 //adding student in the database
+const entry = { name: "Jack", age: 17 };
 app.post("/", (req, res) => {
   //code logic for creating a student
 
   //Create operation
-  const entry = { name: "Jack", age: 17 };
-  student.insertOne(entry, (err, result) => {
-    if (err) {
+ 
+  student
+    .insertOne(entry)
+    .then((result) => {
+      console.log("new student has been added: " + result);
+      res.send({ message: "user added successfully" });
+    })
+    .catch((err) => {
       console.log("An error occurred: " + err);
-      res.send({"error":"An error occurred"+err})
-      return;
-    }
-    console.log("new student has been added: " + result);
-    res.send({"message": "user added successfully"});
-  });
+      res.send({ error: "An error occurred" + err });
+    });
+
+  // student.insertOne(entry, (err, result) => {
+  //   if (err) {
+  //     console.log("An error occurred: " + err);
+  //     res.send({"error":"An error occurred"+err})
+  //     return;
+  //   }
+  //   console.log("new student has been added: " + result);
+  //   res.send({"message": "user added successfully"});
+  // });
 });
 
 //Retrieving students from database
 app.get("/", (req, res) => {
   //code logic for retrieving the students from the database
-    // code logic for retrieving one student
+  // code logic for retrieving one student
   // Retriveing data from the database
-student.find({ name: "Jack" }, (err, result) => {
-  if (err) {
-    console.error("Error reading document:", err);
-    res.send({"error":"sorry an error occured"+err});
-    return;
-  }
-  console.log("Found student:", result);
-  res.send({"message":"Students have been found"});
-});
+
+  student
+    .find(entry)
+    .toArray()
+    .then((result) => {
+      console.log("Found student:", result);
+      res.send({ message: "Students have been found" });
+    })
+    .catch((error) => {
+      console.error("Error reading document:", error);
+      res.send({ error: "sorry an error occured" + error });
+    });
 });
 
 //Retrieving one student from database with an id
 app.get("/:id", (req, res) => {
   // code logic for retrieving one student
   // Retriveing data from the database
-student.findOne({ name: "Jack" }, (err, result) => {
-  if (err) {
-    console.error("Error reading document:", err);
-    res.send({"error":" Could not find student with this id"+err})
-    return;
-  }
-  console.log("Found student:", result);
-  res.send({"message":"Found student with this id"});
-});
 
+  student
+    .findOne({ name: "Jack" })
+    .then((result) => {
+      console.log("Found student:", result);
+      res.send({ message: "Found student with this id" });
+    })
+    .catch((err) => {
+      console.error("Error reading document:", err);
+      res.send({ error: " Could not find student with this id" + err });
+    });
 });
 
 //Updating students from database the database with an id
-app.patch("/:id", (req, res) => {
+app.patch("/:name", (req, res) => {
   // Updating data in the database
 
-student.updateOne({ name: "Jack" }, { $set: { age: 21 } }, (err, result) => {
-  if (err) {
-    console.log("Sorry an error occured" + err);
-    res.send({"error":"Sorry an error occured" + err});
-  }
-  console.log("Successfully updated" + result);
-  res.send({"message":"Student updated successfully!"});
-});
+  student
+    .updateOne({ name: "Jack" }, { $set: { age: 21 } })
+    .then((result) => {
+      console.log("Successfully updated" + result);
+      res.send({ message: "Student updated successfully!" });
+    })
+    .catch((error) => {
+      console.log("Sorry an error occured" + error);
+      res.send({ error: "Sorry an error occured" + error });
+    });
+
+  
 });
 //deleting a specific student
-app.delete("/:id", (req, res) => {
+app.delete("/:name", (req, res) => {
   // code logic for deleting a specific student
 
   //delete data in the database
-student.deleteOne({ name: "Jack" }, (err, result) => {
-  if (err) {
-    console.log("Sorry an error occured" + err);
-    res.send({"error":"Sorry an error has occured" + err})
+  student.deleteOne({name: "student"}).then((result)=>{
+    console.log("Successfully deleted " + result);
+    res.send({ "message ": "successfully deleted" });
+  }).catch((error) => {
+    res.send({ "error": "Sorry an error has occured" + error });
   }
-  console.log("Successfully deleted " + result);
-  res.send({"message ":"successfully deleted"});
-});
+  );
 
 });
-
+//connection.close();
 app.listen(port, () => {
   console.log("Server listening on port " + port);
 });
