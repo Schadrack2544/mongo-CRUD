c0fb226a86bb97cbc69479b53b14225499d38a6f
Schadrack2544 <nzayinaschad250@gmail.com>
2023-09-06 10:37:32 +0200

    adding students collenction to endpoints

--- index.js
+++ index.js
@@ -1,88 +1,108 @@
 //const express=require('express');
-import express from 'express';
-import mongodb from 'mongodb';
+import express from "express";
+import mongodb from "mongodb";
 
-const mongoclient=mongodb.MongoClient;
-const connectionString='mongodb://127.0.0.1:27017/school';
+const mongoclient = mongodb.MongoClient;
+const connectionString = "mongodb://127.0.0.1:27017/school";
 //initiating the connection
-const connection=new mongoclient(connectionString);
+const connection = new mongoclient(connectionString);
 
-connection.connect((err)=>{
-    if(err){
-console.log("Error connecting to mongodb: " + err);
-    } 
-
-}).then(()=>{
-    console.log("Connection established")
-});
+connection
+  .connect((err) => {
+    if (err) {
+      console.log("Error connecting to mongodb: " + err);
+    }
+  })
+  .then(() => {
+    console.log("Connection established");
+  });
 
 //Database as school and table students
 
-const student =connection.db('school').collection('students');
+const student = connection.db("school").collection("students");
 //CRUD Operations
-//Create operation
-const entry={name:"Jack",age:17}
-
-student.insertOne(entry,(err,result)=>{
-    if(err){
-        console.log("An error occurred: " + err)
-        return;
-    }
-    console.log("new student has been added: " + result);
-});
-
-// Retriveing data from the database
-student.findOne({name:"Jack"},(err,result)=>{
-    if (err) {
-        console.error('Error reading document:', err);
-        return;
-      }
-      console.log('Found student:', result);
-})
-
-// Updating data in the database
-
-student.updateOne({name: 'Jack'},{$set:{age:21}},(err,result)=>{
-   if(err){
-    console.log("Sorry an error occured"+err);
-   }
-   console.log("Successfully updated"+result);
-});
-
-//delete data in the database
-student.deleteOne({name: 'Jack'},(err,result)=>{
-    if(err){
-        console.log("Sorry an error occured"+err);
-       }
-       console.log("Successfully deleted "+result);
-});
-
 connection.close();
 
+const app = express();
+const port = 4000;
 
+//adding student in the database
+app.post("/", (req, res) => {
+  //code logic for creating a student
 
+  //Create operation
+  const entry = { name: "Jack", age: 17 };
+  student.insertOne(entry, (err, result) => {
+    if (err) {
+      console.log("An error occurred: " + err);
+      res.send({"error":"An error occurred"+err})
+      return;
+    }
+    console.log("new student has been added: " + result);
+    res.send({"message": "user added successfully"});
+  });
+});
 
+//Retrieving students from database
+app.get("/", (req, res) => {
+  //code logic for retrieving the students from the database
+    // code logic for retrieving one student
+  // Retriveing data from the database
+student.find({ name: "Jack" }, (err, result) => {
+  if (err) {
+    console.error("Error reading document:", err);
+    res.send({"error":"sorry an error occured"+err});
+    return;
+  }
+  console.log("Found student:", result);
+  res.send({"message":"Students have been found"});
+});
 
+//Retrieving one student from database with an id
+app.get("/:id", (req, res) => {
+  // code logic for retrieving one student
+  // Retriveing data from the database
+student.findOne({ name: "Jack" }, (err, result) => {
+  if (err) {
+    console.error("Error reading document:", err);
+    res.send({"error":" Could not find student with this id"+err})
+    return;
+  }
+  console.log("Found student:", result);
+  res.send({"message":"Found student with this id"});
+});
 
+});
 
+//Updating students from database the database with an id
+app.patch("/:id", (req, res) => {
+  // Updating data in the database
 
-const app=express();
-const port =4000;
+student.updateOne({ name: "Jack" }, { $set: { age: 21 } }, (err, result) => {
+  if (err) {
+    console.log("Sorry an error occured" + err);
+    res.send({"error":"Sorry an error occured" + err});
+  }
+  console.log("Successfully updated" + result);
+  res.send({"message":"Student updated successfully!"});
+});
+});
+//deleting a specific student
+app.delete("/:id", (req, res) => {
+  // code logic for deleting a specific student
 
-// app.get('/', (req, res) => {
-//        res.send('<h1 style="color:red">Welcome to the new world</h1>');
-// });
+  //delete data in the database
+student.deleteOne({ name: "Jack" }, (err, result) => {
+  if (err) {
+    console.log("Sorry an error occured" + err);
+    res.send({"error":"Sorry an error has occured" + err})
+  }
+  console.log("Successfully deleted " + result);
+  res.send({"message ":"successfully deleted"});
+});
 
-// app.get('/about', (req, res) => {
-//     res.send('<h3>We are good students liking to study</h3>');
-// });
+});
 
-// app.get('/services', (req, res) => {
-//     res.send('<h3>Offering good learning materials</h3>');
-// });
-
-
-
-app.listen(port,()=>{
-    console.log("Server listening on port "+port)
+app.listen(port, () => {
+  console.log("Server listening on port " + port);
 });
